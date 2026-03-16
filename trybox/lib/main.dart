950d9ee1c5afd80b8298f35f81ee3551ccc0a531
Aalok_kr. <121732315+Aalokkumar0@users.noreply.github.com>
2026-03-16 13:39:27 +0530

    practices

---
+++ trybox/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TryBoxApp());
+}
+
+class TryBoxApp extends StatelessWidget {
+  const TryBoxApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, 
+    home:MyHomePage(),
+    );
+  }
+}
+
+class TryBoxscreen extends StatefulWidget {
+  const TryBoxscreen({super.key});
+
+  @override
+  State<TryBoxscreen> createState() => _TryBoxscreenState();
+}
+
+class _TryBoxscreenState extends State<TryBoxscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(title: Text("write Everything here")),
+      body: Container(height: 200, width: 250, color: Colors.yellow),
+    );
+  }
+}
